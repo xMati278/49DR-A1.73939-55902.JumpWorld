79b041fce998115100b661d0069c8216131ae017
xMati278 <59803172+xMati278@users.noreply.github.com>
2021-02-06 09:45:45 +0100

    Add files via upload
    Dodanie Pause Menu oraz statystyk dla Map Menu

---
+++ GlobalStats.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GlobalStats : MonoBehaviour
+{
+    public int Coin1 = 0;
+    public int Secret1 = 0; 
+    public int Coin2 = 0;
+    public int Secret2 = 0;
+    public int Coin3 = 0;
+    public int Secret3 = 0;
+    public int CoinAll;
+    public int SecretAll;
+
+    public GameObject Coin1Amount;
+    public GameObject Secret1Amount;
+    public GameObject Coin2Amount;
+    public GameObject Secret2Amount;
+    public GameObject Coin3Amount;
+    public GameObject Secret3Amount;
+    public GameObject GlobalCoinAmount;
+    public GameObject GlobalSecretAmount;
+
+    public GameObject Map2;
+    public GameObject Map3;
+
+    public bool Map1Completed = false;
+    public bool Map2Completed = false;
+
+    void Update()
+    {
+        CoinAll = Coin1 + Coin2 + Coin3;
+        SecretAll = Secret1 + Secret2 + Secret3;
+
+        Coin1Amount.GetComponent<Text>().text = Coin1 + " / 10";
+        Secret1Amount.GetComponent<Text>().text = Secret1 + " / 1";
+        Coin2Amount.GetComponent<Text>().text = Coin2 + " / 10";
+        Secret2Amount.GetComponent<Text>().text = Secret2 + " / 1";
+        Coin3Amount.GetComponent<Text>().text = Coin3 + " / 10";
+        Secret3Amount.GetComponent<Text>().text = Secret3 + " / 1";
+        GlobalCoinAmount.GetComponent<Text>().text = CoinAll + "";
+        GlobalSecretAmount.GetComponent<Text>().text = SecretAll + "";
+
+        if (Map1Completed == true)
+        {
+            Map2.GetComponent<Button>().interactable = true;
+            Coin2Amount.gameObject.SetActive(true);
+            Secret2Amount.gameObject.SetActive(true);
+
+        }
+        else if (Map1Completed ==false)
+        {
+            Map2.GetComponent<Button>().interactable = false;
+            Coin2Amount.gameObject.SetActive(false);
+            Secret2Amount.gameObject.SetActive(false);
+
+
+        }
+        if (Map2Completed == true && Map1Completed == true)
+        {
+            Map3.GetComponent<Button>().interactable = true;
+            Coin3Amount.gameObject.SetActive(true);
+            Secret3Amount.gameObject.SetActive(true);
+
+
+        }
+        else if( Map2Completed == false)
+        {
+            Map3.GetComponent<Button>().interactable = false;
+            Coin3Amount.gameObject.SetActive(false);
+            Secret3Amount.gameObject.SetActive(false);
+
+
+
+        }
+    }
+}
---
+++ PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Reset1()
+    {
+        SceneManager.LoadScene(1);
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Reset2()
+    {
+        SceneManager.LoadScene(3);
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Reset3()
+    {
+        SceneManager.LoadScene(4);
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(2);
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
