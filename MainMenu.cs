a12772339737a9c7e12b4677f3fa82ca5708166a
Karol Cukierski <57270862+karol870@users.noreply.github.com>
2021-02-06 09:50:48 +0100

    Dodanie skryptów w C#

---
+++ MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static bool isLoading = false;
+    public void NewGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadGame()
+    {
+        isLoading = true;
+        SceneManager.LoadScene(2);
+    }
+    public void Options()
+    {
+        //SceneManager.LoadScene();
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ MapMenuScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapMenuScenes : MonoBehaviour
+{
+    public void Map1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Map2()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Map3()
+    {
+        SceneManager.LoadScene(4);
+
+    }
+}
